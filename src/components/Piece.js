1f2a7a151a32ef7cfceba615d4f5f133ff237325
Matthew Read <mattread90@gmail.com>
2019-09-26 00:27:25 +0100

    💄 Edit emoji colours

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -5,27 +5,29 @@ import styles from "./Piece.module.scss";
 
 const sourceMap = {
   white: {
+    p: { emoji: "♟" },
     R: { emoji: "♜" },
     N: { emoji: "♞" },
     B: { emoji: "♝" },
     Q: { emoji: "♛" },
-    K: { emoji: "♚" },
-    p: { emoji: "♟" }
+    K: { emoji: "♚" }
   },
   black: {
+    p: { emoji: "♟" },
     R: { emoji: "♜" },
     N: { emoji: "♞" },
     B: { emoji: "♝" },
     Q: { emoji: "♛" },
-    K: { emoji: "♚" },
-    p: { emoji: "♟" }
+    K: { emoji: "♚" }
   }
 };
 
 export const Piece = ({ piece: { piece, color } }) => {
   if (!piece) return null;
-  const { image, emoji } = sourceMap[color][piece];
-  if (image) {
+  const { SVG, image, emoji } = sourceMap[color][piece];
+  if (SVG) {
+    return <SVG />;
+  } else if (image) {
     return (
       <img className={styles.image} src={image} alt={`${color} ${piece}`} />
     );
